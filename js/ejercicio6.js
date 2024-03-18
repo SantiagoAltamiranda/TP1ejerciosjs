a7370cad9328fa88a13d8bb58714e86d1f10431a
Santiago Altamiranda <santiagoaltamiranda1997@gmail.com>
2024-03-18 20:20:09 -0300

    agregando cambios

--- js/ejercicio6.js
+++ js/ejercicio6.js
@@ -1,10 +1,10 @@
-let num1 = parseInt(prompt("ingresa el primer número:"));
-let num2 = parseInt(prompt("ingresa el segundo número:"));
+let num1 = parseInt(prompt("ingresa el primer número: "));
+let num2 = parseInt(prompt("ingresa el segundo número: "));
 
 if (num1 > num2) {
-    alert("El " + num1 + " es el número más grande.");
+    document.write("El " + num1 + " es el número más grande. ");
 } else if (num2 > num1) {
-    alert("El " + num2 + " es el número más grande.");
+    document.write("El " + num2 + " es el número más grande. ");
 } else {
-    alert("Los dos números son iguales.");
+    document.write("Los dos números son iguales. ");
 }
